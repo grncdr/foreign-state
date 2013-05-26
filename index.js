7c10fe91ebc252583b6134b86b1eb7c1b9985bc3
Stephen Sugden <me@stephensugden.com>
2013-05-26 14:33:05 -0700

    Use amdefine for AMD compatibility

--- index.js
+++ index.js
@@ -1,45 +1,48 @@
-module.exports = Tracker;
+if (typeof define !== 'function') { var define = require('amdefine')(module) }
 
-var deepCopy = require('deep-copy');
+define(function (require) {
+  var deepCopy = require('deep-copy');
+  return Tracker;
 
-function Tracker(hiddenPropertyName) {
-  if (!(this instanceof Tracker)) {
-    return new Tracker(hiddenPropertyName);
-  }
-  // A unique sentinel value used to guard access to hidden state
-  var key = function () {};
-  
-  hiddenPropertyName = hiddenPropertyName || '__getForeignState';
-  
-  this.track = function (object, initialState) {
-    takeOwnership(object, key, hiddenPropertyName, initialState);
-    return this.getState(object);
-  }
-  
-  this.getState = function (object) {
-    return object[hiddenPropertyName](key);
-  }
-}
-
-function takeOwnership(object, key, property, initialState) {
-  if (object.hasOwnProperty(property)) {
-    // will throw an error if we aren't the manager for the object.
-    object[property](key);
-  } else {
-    // add non-enumerable, non-configurable, non-writable property
-    Object.defineProperty(object, property, {
-      '__proto__': null,
-      'value': initForeignState(key, initialState)
-    });
-  }
-}
-
-function initForeignState(ownerKey, initialState) {
-  var foreignState = deepCopy(initialState || {});
-  return function (key) {
-    if (key !== ownerKey) {
-      throw new Error("Only this objects owner may access it's foreign state");
+  function Tracker(hiddenPropertyName) {
+    if (!(this instanceof Tracker)) {
+      return new Tracker(hiddenPropertyName);
     }
-    return foreignState;
+    // A unique sentinel value used to guard access to hidden state
+    var key = function () {};
+    
+    hiddenPropertyName = hiddenPropertyName || '__getForeignState';
+    
+    this.track = function (object, initialState) {
+      takeOwnership(object, key, hiddenPropertyName, initialState);
+      return this.getState(object);
+    }
+    
+    this.getState = function (object) {
+      return object[hiddenPropertyName](key);
+    }
   }
-}
+
+  function takeOwnership(object, key, property, initialState) {
+    if (object.hasOwnProperty(property)) {
+      // will throw an error if we aren't the manager for the object.
+      object[property](key);
+    } else {
+      // add non-enumerable, non-configurable, non-writable property
+      Object.defineProperty(object, property, {
+        '__proto__': null,
+        'value': initForeignState(key, initialState)
+      });
+    }
+  }
+
+  function initForeignState(ownerKey, initialState) {
+    var foreignState = deepCopy(initialState || {});
+    return function (key) {
+      if (key !== ownerKey) {
+        throw new Error("Only this objects owner may access it's foreign state");
+      }
+      return foreignState;
+    }
+  }
+})
